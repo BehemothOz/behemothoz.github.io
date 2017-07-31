dbece80342a5c72afc8ebf20557e55695656779b
BehemothOz <BehemothOz@yandex.ru>
2017-07-31 11:33:32 +0300

    Add files via upload

---
+++ bnr/js/main.js
@@ -0,0 +1,88 @@
+"use strict";
+
+
+
+
+
+// Lang popup
+
+;(function() {
+    const langToggle = document.querySelector('.lang-toggle'),
+          langList = document.querySelector('.lang-list'),
+          body = document.querySelector('.body'),
+          google = document.querySelector('#google_translate_element');
+
+    langToggle.addEventListener('click', function(e) {
+        e.preventDefault();
+        this.classList.toggle('open');
+
+        langList.classList.toggle('visible');
+    })
+
+    body.addEventListener('click', function(e) {
+        const parent = e.target.parentElement;
+
+        if (e.target !== langToggle && langToggle.classList.contains('open')
+            && langList.classList.contains('visible')
+            && parent.parentElement !== google) {
+
+            langToggle.classList.remove('open');
+            langList.classList.remove('visible');
+        }
+    })
+
+})();
+
+
+
+// Lang item
+;(function() {
+    const langItem = document.querySelectorAll('.lang-item-path');
+
+    // function delete_cookie(name) {
+    //     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
+    // };
+
+    function handleClick(e) {
+        e.preventDefault();
+        const cookieArr = document.cookie.split(';');
+
+        for (let i = 0; i < cookieArr.length; i++) {
+            let key = cookieArr[i].split('=')[0].trim();
+
+            if ( key === 'googtrans' ) {
+                // delete_cookie(key);
+                // Cookies.remove(key, { path: "/",domain: ".localhost.com" });
+                // Cookies.remove(key, { path: '' });
+                // eraseCookie(key, "..com"); //erases the .one.om domain cookie
+                eraseCookie(key, ""); //erases the www.one.om domain cookie
+            }
+        }
+
+        window.location.href = e.target.firstElementChild.getAttribute('href');
+    };
+
+    langItem.forEach(function(el, i) {
+        el.addEventListener('click', handleClick);
+    })
+})();
+
+
+
+function createCookie(name, value, days, domain) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; domain=" + domain + "; path=/";
+}
+
+function eraseCookie(name, domain) {
+    createCookie(name, "", -1, domain);
+}
+
+// eraseCookie("googtrans", ".one.om"); //erases the .one.om domain cookie
+// eraseCookie("googtrans", ""); //erases the www.one.om domain cookie
