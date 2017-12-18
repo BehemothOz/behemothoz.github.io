19bc67d5f816ce725db3f692e763982795c1bea0
BehemothOz <BehemothOz@yandex.ru>
2017-12-18 20:27:15 +0300

    update

--- todo/main.bundle.js
+++ todo/main.bundle.js
@@ -20,7 +20,7 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy re
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<body class=\"m-a-1\">\n  <!-- Title -->\n  <h3 class=\"bg-info p-a-1\">{{ getName() }}</h3>\n\n  <!-- Add task -->\n  <div class=\"m-t-1 m-b-1\">\n    <input class=\"form-control\" #todoForm>\n    <button class=\"btn btn-success m-t-1\" (click)=\"addNewTodo(todoForm)\">Add</button>\n  </div>\n\n  <!-- View task -->\n  <table class=\"table table-striped table-bordered\">\n    <thead>\n      <tr>\n        <th></th>\n        <th>Description</th>\n        <th>Done</th>\n        <th></th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr *ngFor=\"let item of getTodoItems(); let i = index\">\n        <td>{{ i + 1 }}</td>\n        <td>{{ item.action }}</td>\n        <td><input type=\"checkbox\" [(ngModel)]=\"item.done\"></td>\n        <td [ngSwitch]=\"item.done\">\n          <span *ngSwitchCase=\"true\">Yes</span>\n          <span *ngSwitchDefault>No</span>\n        </td>\n      </tr>\n    </tbody>\n  </table>\n</body>\n\n"
+module.exports = "<body class=\"body m-a-1\">\n  <div class=\"container\">\n    <!-- Title -->\n    <h3 class=\"title bg-info p-a-1\">Приветствую, {{ getName() }}!</h3>\n\n    <button\n      *ngIf=\"!shownTodoForm\"\n      class=\"btn btn-primary m-t-1 m-b-1\"\n      (click)=\"toggleTodoForm()\">Добавить новую задачу</button>\n\n    <!-- Add task -->\n    <div *ngIf=\"shownTodoForm\" class=\"m-t-1 m-b-1\">\n      <input class=\"form-control\" #todoForm>\n      <button class=\"btn btn-success m-t-1\" (click)=\"addNewTodo(todoForm)\">Добавить</button>\n      <button class=\"btn btn-secondary m-t-1\" (click)=\"toggleTodoForm()\">Отмена</button>\n    </div>\n\n    <!-- View task -->\n    <table *ngIf=\"todoItems.length\" class=\"table table-striped table-bordered\">\n      <thead>\n        <tr>\n          <th></th>\n          <th>Описание</th>\n          <th>Статус</th>\n          <th></th>\n        </tr>\n      </thead>\n      <tbody>\n        <tr *ngFor=\"let item of todoItems; let i = index\">\n          <td class=\"cell-index\">{{ i + 1 }}</td>\n          <td>{{ item.action }}</td>\n          <td class=\"cell-status\">\n            <label class=\"label\">\n              <input class=\"input\" type=\"checkbox\" [(ngModel)]=\"item.done\">\n              <span class=\"status\"></span>\n            </label>\n          </td>\n          <td class=\"cell-del\">\n            <span class=\"delete\" (click)=\"delTodoItem(item)\"></span>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n\n    <p *ngIf=\"!todoItems.length\">У Вас нет ни одной задачи. Добавьте ее по кнопке выше.</p>\n  </div>\n</body>\n\n"
 
 /***/ }),
 
@@ -32,7 +32,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "", ""]);
+exports.push([module.i, ".container {\n  max-width: 1280px;\n  margin: 0 auto; }\n\n.title {\n  margin-bottom: 0; }\n\n.cell-index {\n  width: 50px;\n  text-align: center; }\n\n.cell-del {\n  position: relative;\n  width: 50px; }\n  .cell-del .delete {\n    position: absolute;\n    top: 0;\n    left: 0;\n    bottom: 0;\n    right: 0;\n    background: url(\"data:image/svg+xml,%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width%3D%2216%22 height%3D%2220%22 viewBox%3D%220 0 16 20%22%3E    %3Cg fill%3D%22%235A595E%22 fill-rule%3D%22nonzero%22%3E        %3Cpath d%3D%22M5.274 6.027h1.22v9.761h-1.22zM7.534 6.027h1.22v9.761h-1.22zM9.795 6.027h1.22v9.761h-1.22zM0 2.26h15.861v1.22H0z%22%2F%3E        %3Cpath d%3D%22M11.293 3.01h-1.139V1.79a.642.642 0 0 0-.65-.651h-2.44a.642.642 0 0 0-.651.65v1.22H5.274V1.79C5.274.813 6.087 0 7.064 0h2.44c.976 0 1.79.813 1.79 1.79v1.22M11.878 19.526h-7.32c-.977 0-1.831-.814-1.912-1.79L1.506 3.095l1.22-.081 1.14 14.64c.04.367.366.652.691.652h7.32c.367 0 .692-.326.692-.651l1.139-14.641 1.22.081-1.139 14.641c-.081 1.017-.935 1.79-1.911 1.79%22%2F%3E    %3C%2Fg%3E%3C%2Fsvg%3E\") no-repeat center;\n    cursor: pointer; }\n    .cell-del .delete:hover {\n      background: url(\"data:image/svg+xml,%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width%3D%2216%22 height%3D%2220%22 viewBox%3D%220 0 16 20%22%3E    %3Cg fill%3D%22%23E94457%22 fill-rule%3D%22nonzero%22 opacity%3D%22.7%22%3E        %3Cpath d%3D%22M5.274 6.027h1.22v9.761h-1.22zM7.534 6.027h1.22v9.761h-1.22zM9.795 6.027h1.22v9.761h-1.22zM0 2.26h15.861v1.22H0z%22%2F%3E        %3Cpath d%3D%22M11.293 3.01h-1.139V1.79a.642.642 0 0 0-.65-.651h-2.44a.642.642 0 0 0-.651.65v1.22H5.274V1.79C5.274.813 6.087 0 7.064 0h2.44c.976 0 1.79.813 1.79 1.79v1.22M11.878 19.526h-7.32c-.977 0-1.831-.814-1.912-1.79L1.506 3.095l1.22-.081 1.14 14.64c.04.367.366.652.691.652h7.32c.367 0 .692-.326.692-.651l1.139-14.641 1.22.081-1.139 14.641c-.081 1.017-.935 1.79-1.911 1.79%22%2F%3E    %3C%2Fg%3E%3C%2Fsvg%3E\") no-repeat center; }\n\n.cell-status {\n  position: relative;\n  width: 100px; }\n  .cell-status .label {\n    position: absolute;\n    top: 0;\n    left: 0;\n    bottom: 0;\n    right: 0;\n    margin-bottom: 0;\n    cursor: pointer; }\n  .cell-status .input {\n    display: none; }\n  .cell-status .input:checked + .status {\n    background: url(\"data:image/svg+xml,%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width%3D%2222%22 height%3D%2222%22 viewBox%3D%220 0 22 22%22%3E    %3Cg fill%3D%22none%22 fill-rule%3D%22evenodd%22%3E        %3Ccircle cx%3D%2211%22 cy%3D%2211%22 r%3D%2211%22 fill%3D%22%234990E2%22%2F%3E        %3Cpath stroke%3D%22%23FFF%22 stroke-linecap%3D%22round%22 stroke-linejoin%3D%22round%22 stroke-width%3D%222%22 d%3D%22M6.162 11.164l3.269 3.269 6.285-6.285%22%2F%3E    %3C%2Fg%3E%3C%2Fsvg%3E\") no-repeat center; }\n  .cell-status .status {\n    position: absolute;\n    display: block;\n    width: 100%;\n    height: 100%;\n    background: url(\"data:image/svg+xml,%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 width%3D%2222%22 height%3D%2222%22 viewBox%3D%220 0 22 22%22%3E    %3Cg fill%3D%22none%22 fill-rule%3D%22evenodd%22 stroke%3D%22%23C1C1C1%22%3E        %3Ccircle cx%3D%2211%22 cy%3D%2211%22 r%3D%2210.5%22%2F%3E        %3Cpath stroke-linecap%3D%22round%22 stroke-linejoin%3D%22round%22 stroke-width%3D%222%22 d%3D%22M6.162 11.164l3.269 3.269 6.285-6.285%22%2F%3E    %3C%2Fg%3E%3C%2Fsvg%3E\") no-repeat center; }\n", ""]);
 
 // exports
 
@@ -60,18 +60,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var AppComponent = (function () {
     function AppComponent() {
         this.model = new __WEBPACK_IMPORTED_MODULE_1__model__["a" /* Model */];
+        this.todoItems = this.model.items;
+        this.shownTodoForm = false;
     }
     AppComponent.prototype.getName = function () {
         return this.model.user;
     };
-    AppComponent.prototype.getTodoItems = function () {
-        return this.model.items.filter(function (item) { return !item.done; });
+    AppComponent.prototype.toggleTodoForm = function () {
+        this.shownTodoForm = !this.shownTodoForm;
+    };
+    AppComponent.prototype.delTodoItem = function (todoItem1) {
+        var newTodoItems = this.todoItems.filter(function (item) { return item.id != todoItem1.id; });
+        this.todoItems = newTodoItems;
     };
     AppComponent.prototype.addNewTodo = function (todoForm) {
         if (todoForm.value !== "") {
-            this.model.items.push(new __WEBPACK_IMPORTED_MODULE_1__model__["b" /* TodoItem */](todoForm.value, false));
+            this.todoItems.push(new __WEBPACK_IMPORTED_MODULE_1__model__["b" /* TodoItem */](this.generateRandomId(), todoForm.value, false));
             todoForm.value = "";
         }
+    };
+    AppComponent.prototype.generateRandomId = function () {
+        return Math.floor((1 + Math.random()) * 0x10000);
     };
     AppComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
@@ -137,19 +146,20 @@ var AppModule = (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return TodoItem; });
 var Model = (function () {
     function Model() {
-        this.user = "Denis";
+        this.user = "\u0410nonymous";
         this.items = [
-            new TodoItem("\u041A\u0443\u043F\u0438\u0442\u044C \u0431\u0438\u0442\u043A\u043E\u0438\u043D\u044B \u0443 \u0446\u044B\u0433\u0430\u043D", false),
-            new TodoItem("\u041F\u043E\u0437\u043D\u0430\u0442\u044C \u0434\u0437\u0435\u043D \u0438 Angular", false),
-            new TodoItem("\u0423\u043A\u0440\u0430\u0441\u0442\u044C \u043B\u043E\u0448\u0430\u0434\u044C", false),
-            new TodoItem("\u041F\u043E\u043C\u0435\u043D\u044F\u0442\u044C \u0441\u0442\u0440\u0443\u043D\u044B", false)
+            new TodoItem(0, "\u041A\u0443\u043F\u0438\u0442\u044C \u0431\u0438\u0442\u043A\u043E\u0438\u043D\u044B \u0443 \u0446\u044B\u0433\u0430\u043D", false),
+            new TodoItem(1, "\u041F\u043E\u0437\u043D\u0430\u0442\u044C \u0434\u0437\u0435\u043D \u0438 Angular", false),
+            new TodoItem(2, "\u0423\u043A\u0440\u0430\u0441\u0442\u044C \u043B\u043E\u0448\u0430\u0434\u044C", false),
+            new TodoItem(3, "\u041F\u043E\u043C\u0435\u043D\u044F\u0442\u044C \u0441\u0442\u0440\u0443\u043D\u044B", false)
         ];
     }
     return Model;
 }());
 
 var TodoItem = (function () {
-    function TodoItem(action, done) {
+    function TodoItem(id, action, done) {
+        this.id = id;
         this.action = action;
         this.done = done;
     }
