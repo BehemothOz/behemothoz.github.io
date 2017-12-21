b14cbc9fbcf3e5729704cebde9af603d63e1af60
BehemothOz <BehemothOz@yandex.ru>
2017-12-21 15:58:56 +0300

    testing 2

--- cct/js/main.js
+++ cct/js/main.js
@@ -1,50 +1,14 @@
 "use strict";
 
 ;(function() {
-  // let header = document.querySelector('.header');
-  // let fixed = false;
-  // let fixedPoint = getDistance();
+  const TOPDIST = 228; // TODO
 
-  // function getDistance() {
-  //   return header.offsetTop;
-  // }
+  window.onscroll = function() {
+    let pageY = window.pageYOffset || document.documentElement.scrollTop;
+    let headerTop = document.querySelector('.header');
 
-  // window.onscroll = function() {
-  //   let pageY = window.pageYOffset || document.documentElement.scrollTop;
-  //   let distance = getDistance() - pageY;
-
-  //   if ( (distance <= 0) && !fixed ) {
-  //     header.classList.add('fixed');
-  //     fixed = true;
-  //   }
-
-  //   else if ( (pageY <= fixedPoint) && fixed ) {
-  //     header.classList.remove('fixed');
-  //     fixed = false;
-  //   }
-  // }
-
-  let h = document.querySelector('.header');
-  // var h = document.getElementById("header");
-  var stuck = false;
-  var stickPoint = getDistance();
-
-  function getDistance() {
-    var topDist = h.offsetTop;
-    return topDist;
-  }
-
-  window.onscroll = function(e) {
-    var distance = getDistance() - window.pageYOffset;
-    var offset = window.pageYOffset;
-    if ( (distance <= 0) && !stuck) {
-      h.style.position = 'fixed';
-      h.style.top = '0px';
-      stuck = true;
-    } else if (stuck && (offset <= stickPoint)){
-      h.style.position = 'static';
-      stuck = false;
-    }
-  }
-
+    ( pageY >= TOPDIST )
+            ? headerTop.classList.add('fixed')
+            : headerTop.classList.remove('fixed');
+  };
 })();
