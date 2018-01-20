061d4dd2fc816ffd4b22985196c6d60ce19726ab
BehemothOz <BehemothOz@yandex.ru>
2018-01-20 17:28:29 +0300

    second commit

--- notes/main.bundle.js
+++ notes/main.bundle.js
@@ -43,7 +43,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var routes = [
     { path: '', pathMatch: 'full', redirectTo: 'notes' },
     { path: 'notes', component: __WEBPACK_IMPORTED_MODULE_3__notes_notes_component__["a" /* NotesComponent */] },
-    { path: 'form', component: __WEBPACK_IMPORTED_MODULE_5__form_form_component__["a" /* FormComponent */] },
+    { path: 'notes/create', component: __WEBPACK_IMPORTED_MODULE_5__form_form_component__["a" /* FormComponent */] },
     { path: 'settings', component: __WEBPACK_IMPORTED_MODULE_4__settings_settings_component__["a" /* SettingsComponent */] }
 ];
 var AppRoutingModule = (function () {
@@ -69,7 +69,7 @@ var AppRoutingModule = (function () {
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<header class=\"header\">\n  <h1 class=\"logo\">Notes</h1>\n  <a class=\"link-setting\"  routerLink=\"settings\"></a>\n</header>\n\n<div class=\"container\">\n  <router-outlet></router-outlet>\n\n  <a class=\"add-note\" routerLink=\"form\"></a>\n</div>"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div class=\"main-container\">\n  <header class=\"header\">\n    <h1 class=\"logo\">Notes</h1>\n    <a class=\"link-setting\"  routerLink=\"settings\"></a>\n  </header>\n\n  <!-- View routing -->\n  <router-outlet></router-outlet>\n</div>"
 
 /***/ }),
 
@@ -181,7 +181,7 @@ var AppModule = (function () {
 /***/ "../../../../../src/app/form/form.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "\n<label for=\"title\">Заголовок</label>\n<input type=\"text\" id=\"title\" #noteTitle>\n\n<br>\n\n<label for=\"desc\">Описание</label>\n<input type=\"text\" id=\"desc\" #noteDesc>\n\n<br>\n\n\n<div class=\"control-panel\">\n  <button (click)=\"onSave(noteTitle.value, noteDesc.value)\">Добавить</button>\n  <button (click)=\"onCancel()\">Отмена</button>\n  <button>Удалить</button>\n</div>\n"
+module.exports = "<div class=\"container container-form\">\n  <div class=\"form-group\">\n    <label class=\"label\" for=\"title\">Заголовок</label>\n    <input class=\"input\" type=\"text\" id=\"title\" #noteTitle>\n  </div>\n\n  <div class=\"form-group\">\n      <div class=\"label\">Фон заголовка</div>\n      <label class=\"radio-color\">\n        <input \n          class=\"input-color\"\n          type=\"radio\"\n          name=\"bg-color\"\n          value=\"orange\"\n          checked\n          #radioOrange\n          (change)=\"setBgTitleColor(radioOrange.value)\">\n        <span class=\"color orange\"></span>\n      </label>\n      <label class=\"radio-color\">\n        <input\n          class=\"input-color\"\n          type=\"radio\"\n          name=\"bg-color\"\n          value=\"blue\"\n          #radioBlue\n          (change)=\"setBgTitleColor(radioBlue.value)\">\n        <span class=\"color blue\"></span>\n      </label>\n      <label class=\"radio-color\">\n        <input\n          class=\"input-color\"\n          type=\"radio\"\n          name=\"bg-color\"\n          value=\"purple\"\n          #radioPurple\n          (change)=\"setBgTitleColor(radioPurple.value)\">\n        <span class=\"color purple\"></span>\n      </label>\n  </div>\n\n  <div class=\"form-group\">\n    <label class=\"label\" for=\"desc\">Описание</label>\n    <textarea class=\"textarea\" type=\"text\" id=\"desc\" rows=\"12\" #noteDesc></textarea>\n  </div>\n</div>\n\n<div class=\"control-panel\">\n  <button\n    class=\"btn-panel btn-panel-cancel\"\n    (click)=\"onCancel()\"></button>\n  <button\n    class=\"btn-panel btn-panel-save\"\n    (click)=\"onSave(noteTitle.value, noteDesc.value)\"></button>\n  <button\n    class=\"btn-panel btn-panel-delete\"></button>\n</div>\n"
 
 /***/ }),
 
@@ -193,7 +193,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "input {\n  width: 100%;\n  height: 35px; }\n\n.control-panel {\n  position: fixed;\n  bottom: 0;\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  background-color: #F44336;\n  left: 0;\n  width: 100%;\n  height: 50px; }\n", ""]);
+exports.push([module.i, ".container-form {\n  min-height: calc(100vh - 54px - 50px); }\n  .container-form .form-group {\n    position: relative;\n    margin-bottom: 25px; }\n  .container-form .label {\n    display: block;\n    margin-bottom: 15px;\n    font-size: 14px;\n    color: #F44336; }\n  .container-form .input {\n    width: 100%;\n    height: 34px;\n    padding: 0 10px;\n    font-size: 16px;\n    border: none;\n    outline: none;\n    border-bottom: 1px solid #cecdcd;\n    background-color: transparent; }\n    .container-form .input:focus {\n      border-bottom-color: #F44336; }\n  .container-form .radio-color {\n    display: inline-block;\n    vertical-align: middle;\n    margin-right: 4px; }\n    .container-form .radio-color .input-color {\n      display: none; }\n    .container-form .radio-color .input-color:checked + .color {\n      outline: 2px solid #000; }\n    .container-form .radio-color .color {\n      display: inline-block;\n      width: 30px;\n      height: 30px; }\n      .container-form .radio-color .color.orange {\n        background-color: #FF9800; }\n      .container-form .radio-color .color.blue {\n        background-color: #2196F3; }\n      .container-form .radio-color .color.purple {\n        background-color: #9C27B0; }\n  .container-form .textarea {\n    width: 100%;\n    border: none;\n    outline: none;\n    border-bottom: 1px solid #cecdcd;\n    background-color: transparent;\n    resize: none; }\n    .container-form .textarea:focus {\n      border-bottom-color: #F44336; }\n\n.control-panel {\n  position: fixed;\n  bottom: 0;\n  left: 0;\n  display: -webkit-box;\n  display: -ms-flexbox;\n  display: flex;\n  width: 100%;\n  height: 50px;\n  -webkit-box-align: center;\n      -ms-flex-align: center;\n          align-items: center;\n  -ms-flex-pack: distribute;\n      justify-content: space-around;\n  background-color: #F44336; }\n  .control-panel .btn-panel {\n    width: 25px;\n    height: 25px;\n    background-color: transparent;\n    background-repeat: no-repeat;\n    background-position: center;\n    background-size: contain;\n    border: none;\n    outline: none; }\n    .control-panel .btn-panel.btn-panel-save {\n      background-image: url(\"data:image/svg+xml,%3C%3Fxml version%3D%221.0%22 encoding%3D%22iso-8859-1%22%3F%3E%3C!DOCTYPE svg PUBLIC %22-%2F%2FW3C%2F%2FDTD SVG 1.1%2F%2FEN%22 %22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22 version%3D%221.1%22 viewBox%3D%220 0 366 366%22 enable-background%3D%22new 0 0 366 366%22 width%3D%22512px%22 height%3D%22512px%22%3E  %3Cg%3E    %3Cpath d%3D%22M363.071%2C40.359l-37.43-37.43C323.766%2C1.054%2C321.223%2C0%2C318.57%2C0H10C4.477%2C0%2C0%2C4.477%2C0%2C10v346c0%2C5.523%2C4.477%2C10%2C10%2C10h346   c5.522%2C0%2C10-4.477%2C10-10V47.43C366%2C44.778%2C364.946%2C42.234%2C363.071%2C40.359z M72%2C20h26v96H72V20z M118%2C20h150v96H118V20z M268%2C346H72   V193h196V346z M346%2C346h-58V183c0-5.523-4.478-10-10-10H62c-5.523%2C0-10%2C4.477-10%2C10v163H20V20h32v106c0%2C5.523%2C4.477%2C10%2C10%2C10h216   c5.522%2C0%2C10-4.477%2C10-10V20h26.428L346%2C51.572V346z%22 fill%3D%22%23FFFFFF%22%2F%3E    %3Cpath d%3D%22m234%2C33c-5.522%2C0-10%2C4.477-10%2C10v50c0%2C5.523 4.478%2C10 10%2C10s10-4.477 10-10v-50c0-5.523-4.478-10-10-10z%22 fill%3D%22%23FFFFFF%22%2F%3E    %3Cpath d%3D%22m106%2C242h128c5.522%2C0 10-4.477 10-10s-4.478-10-10-10h-128c-5.523%2C0-10%2C4.477-10%2C10s4.477%2C10 10%2C10z%22 fill%3D%22%23FFFFFF%22%2F%3E    %3Cpath d%3D%22m106%2C282h128c5.522%2C0 10-4.477 10-10s-4.478-10-10-10h-128c-5.523%2C0-10%2C4.477-10%2C10s4.477%2C10 10%2C10z%22 fill%3D%22%23FFFFFF%22%2F%3E    %3Cpath d%3D%22m106%2C322h128c5.522%2C0 10-4.477 10-10s-4.478-10-10-10h-128c-5.523%2C0-10%2C4.477-10%2C10s4.477%2C10 10%2C10z%22 fill%3D%22%23FFFFFF%22%2F%3E  %3C%2Fg%3E%3C%2Fsvg%3E\"); }\n    .control-panel .btn-panel.btn-panel-cancel {\n      background-image: url(\"data:image/svg+xml,%3C%3Fxml version%3D%221.0%22 encoding%3D%22iso-8859-1%22%3F%3E%3C!-- Generator%3A Adobe Illustrator 19.0.0%2C SVG Export Plug-In . SVG Version%3A 6.00 Build 0)  --%3E%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22 version%3D%221.1%22 id%3D%22Layer_1%22 x%3D%220px%22 y%3D%220px%22 viewBox%3D%220 0 492 492%22 style%3D%22enable-background%3Anew 0 0 492 492%3B%22 xml%3Aspace%3D%22preserve%22 width%3D%22512px%22 height%3D%22512px%22%3E%3Cg%3E%09%3Cg%3E%09%09%3Cpath d%3D%22M464.344%2C207.418l0.768%2C0.168H135.888l103.496-103.724c5.068-5.064%2C7.848-11.924%2C7.848-19.124    c0-7.2-2.78-14.012-7.848-19.088L223.28%2C49.538c-5.064-5.064-11.812-7.864-19.008-7.864c-7.2%2C0-13.952%2C2.78-19.016%2C7.844    L7.844%2C226.914C2.76%2C231.998-0.02%2C238.77%2C0%2C245.974c-0.02%2C7.244%2C2.76%2C14.02%2C7.844%2C19.096l177.412%2C177.412    c5.064%2C5.06%2C11.812%2C7.844%2C19.016%2C7.844c7.196%2C0%2C13.944-2.788%2C19.008-7.844l16.104-16.112c5.068-5.056%2C7.848-11.808%2C7.848-19.008    c0-7.196-2.78-13.592-7.848-18.652L134.72%2C284.406h329.992c14.828%2C0%2C27.288-12.78%2C27.288-27.6v-22.788    C492%2C219.198%2C479.172%2C207.418%2C464.344%2C207.418z%22 fill%3D%22%23FFFFFF%22%2F%3E%09%3C%2Fg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3C%2Fsvg%3E\"); }\n    .control-panel .btn-panel.btn-panel-delete {\n      background-image: url(\"data:image/svg+xml,%3C%3Fxml version%3D%221.0%22 encoding%3D%22iso-8859-1%22%3F%3E%3C!-- Generator%3A Adobe Illustrator 19.1.0%2C SVG Export Plug-In . SVG Version%3A 6.00 Build 0)  --%3E%3Csvg xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22 xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22 version%3D%221.1%22 id%3D%22Capa_1%22 x%3D%220px%22 y%3D%220px%22 viewBox%3D%220 0 486.4 486.4%22 style%3D%22enable-background%3Anew 0 0 486.4 486.4%3B%22 xml%3Aspace%3D%22preserve%22 width%3D%22512px%22 height%3D%22512px%22%3E%3Cg%3E%09%3Cg%3E%09%09%3Cpath d%3D%22M446%2C70H344.8V53.5c0-29.5-24-53.5-53.5-53.5h-96.2c-29.5%2C0-53.5%2C24-53.5%2C53.5V70H40.4c-7.5%2C0-13.5%2C6-13.5%2C13.5    S32.9%2C97%2C40.4%2C97h24.4v317.2c0%2C39.8%2C32.4%2C72.2%2C72.2%2C72.2h212.4c39.8%2C0%2C72.2-32.4%2C72.2-72.2V97H446c7.5%2C0%2C13.5-6%2C13.5-13.5    S453.5%2C70%2C446%2C70z M168.6%2C53.5c0-14.6%2C11.9-26.5%2C26.5-26.5h96.2c14.6%2C0%2C26.5%2C11.9%2C26.5%2C26.5V70H168.6V53.5z M394.6%2C414.2    c0%2C24.9-20.3%2C45.2-45.2%2C45.2H137c-24.9%2C0-45.2-20.3-45.2-45.2V97h302.9v317.2H394.6z%22 fill%3D%22%23FFFFFF%22%2F%3E%09%09%3Cpath d%3D%22M243.2%2C411c7.5%2C0%2C13.5-6%2C13.5-13.5V158.9c0-7.5-6-13.5-13.5-13.5s-13.5%2C6-13.5%2C13.5v238.5    C229.7%2C404.9%2C235.7%2C411%2C243.2%2C411z%22 fill%3D%22%23FFFFFF%22%2F%3E%09%09%3Cpath d%3D%22M155.1%2C396.1c7.5%2C0%2C13.5-6%2C13.5-13.5V173.7c0-7.5-6-13.5-13.5-13.5s-13.5%2C6-13.5%2C13.5v208.9    C141.6%2C390.1%2C147.7%2C396.1%2C155.1%2C396.1z%22 fill%3D%22%23FFFFFF%22%2F%3E%09%09%3Cpath d%3D%22M331.3%2C396.1c7.5%2C0%2C13.5-6%2C13.5-13.5V173.7c0-7.5-6-13.5-13.5-13.5s-13.5%2C6-13.5%2C13.5v208.9    C317.8%2C390.1%2C323.8%2C396.1%2C331.3%2C396.1z%22 fill%3D%22%23FFFFFF%22%2F%3E%09%3C%2Fg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3Cg%3E%3C%2Fg%3E%3C%2Fsvg%3E\"); }\n", ""]);
 
 // exports
 
@@ -229,14 +229,16 @@ var FormComponent = (function () {
     function FormComponent(service, router) {
         this.service = service;
         this.router = router;
+        this.bgTitleColor = 'orange';
     }
     FormComponent.prototype.ngOnInit = function () {
     };
     FormComponent.prototype.onSave = function (title, desc) {
-        console.log(new __WEBPACK_IMPORTED_MODULE_2__shared_note__["a" /* Note */](title, desc));
-        this.service.create(new __WEBPACK_IMPORTED_MODULE_2__shared_note__["a" /* Note */](title, desc));
-        console.log(this.service.getAll());
+        this.service.create(new __WEBPACK_IMPORTED_MODULE_2__shared_note__["a" /* Note */](title, desc, this.bgTitleColor));
         this.goToNotes();
+    };
+    FormComponent.prototype.setBgTitleColor = function (colorName) {
+        this.bgTitleColor = colorName;
     };
     FormComponent.prototype.onCancel = function () {
         this.goToNotes();
@@ -262,7 +264,7 @@ var FormComponent = (function () {
 /***/ "../../../../../src/app/notes/notes.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"note\" *ngFor=\"let note of notes\">\n  <div class=\"note-header\">\n    <h2 class=\"title\">{{ note.title }}</h2>\n  </div>\n  <div class=\"note-body\">\n    <p class=\"desc\">{{ note.description }}</p>\n  </div>\n</div>"
+module.exports = "<div class=\"container container-notes\">\n  <div class=\"note\" *ngFor=\"let note of notes\">\n    <div [ngClass]=\"['note-header', note.bgTitleColor]\">\n      <h2 class=\"title\">{{ note.title }}</h2>\n    </div>\n    <div class=\"note-body\">\n      <p class=\"desc\">{{ note.description }}</p>\n    </div>\n  </div>\n\n  <a class=\"add-note\" routerLink=\"create\"></a>\n</div>\n"
 
 /***/ }),
 
@@ -274,7 +276,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".note {\n  margin-bottom: 15px;\n  border-radius: 4px;\n  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.24), 0 0 2px rgba(0, 0, 0, 0.12); }\n  .note .note-header {\n    padding: 15px 20px;\n    background-color: #FF9800;\n    border-radius: 4px 4px 0 0; }\n    .note .note-header .title {\n      font-size: 18px;\n      color: #fff; }\n  .note .note-body {\n    padding: 15px 20px;\n    background-color: #fff;\n    border-radius: 0 0 4px 4px; }\n    .note .note-body .desc {\n      font-size: 16px;\n      line-height: 1.3;\n      color: #333333; }\n", ""]);
+exports.push([module.i, ".container-notes {\n  min-height: calc(100vh - 54px); }\n\n.note {\n  margin-bottom: 15px;\n  border-radius: 4px;\n  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.24), 0 0 2px rgba(0, 0, 0, 0.12); }\n  .note .note-header {\n    padding: 15px 20px;\n    border-radius: 4px 4px 0 0; }\n    .note .note-header.orange {\n      background-color: #FF9800; }\n    .note .note-header.blue {\n      background-color: #2196F3; }\n    .note .note-header.purple {\n      background-color: #9C27B0; }\n    .note .note-header .title {\n      font-size: 18px;\n      color: #fff; }\n  .note .note-body {\n    padding: 15px 20px;\n    background-color: #fff;\n    border-radius: 0 0 4px 4px; }\n    .note .note-body .desc {\n      font-size: 16px;\n      line-height: 1.3;\n      color: #333333; }\n", ""]);
 
 // exports
 
@@ -391,9 +393,10 @@ var SettingsComponent = (function () {
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Note; });
 var Note = (function () {
-    function Note(title, description) {
+    function Note(title, description, bgTitleColor) {
         this.title = title;
         this.description = description;
+        this.bgTitleColor = bgTitleColor;
     }
     return Note;
 }());
@@ -415,8 +418,8 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["A" /* Injectable */])();
 var NotesService = (function () {
     function NotesService() {
         this.data = [
-            new __WEBPACK_IMPORTED_MODULE_1__note__["a" /* Note */]('Пример заголовка №1', 'Описание первой записи'),
-            new __WEBPACK_IMPORTED_MODULE_1__note__["a" /* Note */]('Пример заголовка №2', 'Повседневная практика показывает, что начало повседневной работы по формированию позиции влечет за собой процесс внедрения и модернизации позиций, занимаемых участниками в отношении поставленных задач. ')
+            new __WEBPACK_IMPORTED_MODULE_1__note__["a" /* Note */]('Пример заголовка №1', 'Описание первой записи', 'orange'),
+            new __WEBPACK_IMPORTED_MODULE_1__note__["a" /* Note */]('Пример заголовка №2', 'Повседневная практика показывает, что начало повседневной работы по формированию позиции влечет за собой процесс внедрения и модернизации позиций, занимаемых участниками в отношении поставленных задач.', 'blue')
         ];
         // delete(note: Note) {}
     }
