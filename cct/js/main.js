60a9db7a9dcf1eb82f9f82fdb12c93d5619c401e
BehemothOz <BehemothOz@yandex.ru>
2018-01-29 19:27:11 +0300

    15

--- cct/js/main.js
+++ cct/js/main.js
@@ -27,55 +27,18 @@
 })();
 
 
-function initFullPage() {
-  $('#fullpage').fullpage({
-    scrollBar:true,
-    verticalCentered: false,
-    // autoScrolling: true,
-    scrollOverflowReset: false,
-    responsiveHeight: 500,
-  });
-};
-
-
 $(document).ready(function() {
+
+  function initFullPage() {
+    $('#fullpage').fullpage({
+      scrollBar:true,
+      verticalCentered: false,
+      // autoScrolling: true,
+      scrollOverflowReset: false,
+      responsiveHeight: 500,
+    });
+  };
 
   initFullPage();
 
-  $('.btn-down').on('click', function() {
-    $.fn.fullpage.moveSectionDown();
-  });
-
 });
-
-
-
-// ;(function() {
-  
-//   let x = document.querySelector('.section-card');
-//   let cat = document.querySelector('.cat-gif');
-
-//   console.log(x.getBoundingClientRect().top);
-
-//   console.log(cat.style.backgroundImage)
-
-//   function scrollToSection() {
-//     $('html, body').animate({ scrollTop: $(x).offset().top }, 2000);
-//     cat.style.backgroundImage = '';
-//   }
-  
-//   function showCatGif() {
-//     var pageY = window.pageYOffset || document.documentElement.scrollTop;
-
-//     if (pageY === 0) {
-//       cat.style.backgroundImage = 'url("images/cat.gif")';
-//       // setTimeout(scrollToSection, 7000);
-//     }
-//   }
-
-//   window.addEventListener('scroll', showCatGif);
-
-//   // setTimeout(scrollToSection, 7000);
-
-// })();
-
