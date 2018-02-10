6064ca6e5e442d5a9ae47ee460dfb47e32e7a720
BehemothOz <BehemothOz@yandex.ru>
2018-02-10 18:41:55 +0300

    12q

---
+++ tst/js/timer.js
@@ -0,0 +1,28 @@
+// Timer
+
+function startTimer(duration, display) {
+  var timer = duration, 
+      minutes,
+      seconds;
+
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.text(minutes + ":" + seconds);
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+$(window).on('load', function() {
+  var fiveMinutes = 60 * 5,
+      display = $('.timer span');
+
+  startTimer(fiveMinutes, display);
+}) 
